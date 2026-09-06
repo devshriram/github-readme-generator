3f0709de4345b6a26cdfe313b3c5e48ef4295226
Built Different <built.different7813@gmail.com>
2026-09-06 13:14:21 +0530

    Enhance fetchUserDetails with validation and cleanup
    Added username validation and DOM clearing before API call.

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ function fetchUserDetails() {
     // Get the username
     var username = $('#username').val();
 
+    // Validate the username
+    if(username==='') {
+        window.alert('username is required');
+        return;
+    }
+
+    // Clear the DOM
+    $('#content').empty();
+
     // API call to get user details
     $.get(`https://api.github.com/users/${username}`, function(data) {
         var name = data.name;
         var followers = data.followers;
         var following = data.following;
     });
+
 }
 
 $('#button').click(fetchUserDetails);
