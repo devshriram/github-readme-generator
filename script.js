efbd0f0b46c850234c0cda7c4c5c642a811e2084
Built Different <built.different7813@gmail.com>
2026-09-06 15:39:32 +0530

    Add image creation to fetchUserDetails function
    Added functionality to create and append an image to the content after fetching user details.

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function fetchUserDetails() {
 
     // Validate the username
     if(username==='') {
-        window.alert('username is required');
         return;
     }
 
@@ -18,6 +17,11 @@ function fetchUserDetails() {
         var followers = data.followers;
         var following = data.following;
     });
+
+    // Create the README template
+    $(document.createElement('img')).attr('src', "https://raw.githubusercontent.com/BEPb/BEPb/5c63fa170d1cbbb0b1974f05a3dbe6aca3f5b7f3/assets/Bottom_up.svg").css({
+        width: "100%",
+    }).appendTo('#content');
 
 }
 
